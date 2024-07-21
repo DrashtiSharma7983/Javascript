839573ff6aaab06aec40b92ffc288afdea957a7b
Drashti Sharma <86773589+DrashtiSharma7983@users.noreply.github.com>
2024-07-21 11:39:42 +0000

    datatype

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Drashti");
+const accountId = 123;
+let accountEmail = "test@gmail.com";
+var accPass = "123abc";
+accCity = "Tundla";
+let accState;
+
+
+ console.log(accState);
+// console.log(accPass);
+// console.log(accCity);
+
+/*
+prefered not to use var becaause of block scope and functional scope
+*/
+console.table([accountId, accountEmail,accPass,accCity]);
